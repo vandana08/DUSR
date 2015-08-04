b423e8ff1c3d0f9e678a6d12389fc45b8f867223
vandana08 <kvandana1992@gmail.com>
2015-08-04 10:26:44 +0530

    Create LongSumReducer.java

---
+++ USR_n/LongSumReducer.java
@@ -0,0 +1,28 @@
+package USR;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+ 
+public class LongSumReducer extends Reducer<Text, FloatWritable,
+                                                 Text, FloatWritable> {
+ 
+  private double cutoff = 0.8;
+  private FloatWritable result = new FloatWritable();
+  public void reduce(Text key, Iterable<FloatWritable> values,
+                     Context context) throws IOException, InterruptedException {
+	  for (FloatWritable val : values) {
+		  float sim=val.get();
+		  System.out.println("--" + sim);
+		  if(sim >= cutoff)
+		  {
+			  result.set(sim);
+			  context.write(key, result);
+		  }
+	  }
+  
+   
+}
+}
