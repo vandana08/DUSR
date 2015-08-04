7bf0fdfe7bd4a5502543caa292d7ab083f7c22df
vandana08 <kvandana1992@gmail.com>
2015-08-04 10:54:15 +0530

    Create SDFInputFormat.java

---
+++ USR_library/SDFInputFormat.java
@@ -0,0 +1,17 @@
+package USR_library;
+
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+
+public class SDFInputFormat extends TextInputFormat {
+
+    @Override
+    public RecordReader<LongWritable, Text> createRecordReader(InputSplit inputSplit, TaskAttemptContext taskAttemptContext) {
+        return new SDFRecordReader();
+    }
+}
